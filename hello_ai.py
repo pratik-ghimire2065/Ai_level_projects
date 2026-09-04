500b4874389adacf5431fc5b926294cc5fe3172e
Pratik Ghimire <ghimirepratik039@gmail.com>
2026-09-04 07:34:45 +0545

    add function in the code

--- hello_ai.py
+++ hello_ai.py
@@ -2,3 +2,11 @@ print("Learning  ai concepts ")
 print("Just adding one commit to learnm git for practice ")
 print("Part 2 git diff ")
 print("Something happens ")
+def add(a,b):
+    return a +b
+try: 
+    a=int(input("Enter a number : ")) 
+    b=int(input(f"Enter a another number to add with {a}: "))
+except ValueError: 
+    print("Wrong input given ")
+print(f"The sum of {a} and {b} is ",add(a,b))
